a0893f0c2126875af6903ab0b8f1c88f34e6b55e
Egor <egor.vas2004@gmail.com>
2021-10-01 11:08:36 +0300

    added format output

--- Lab3Py/Lab3Py.py
+++ Lab3Py/Lab3Py.py
@@ -7,13 +7,15 @@ while n<=11:
     a=float(x/(math.sqrt(n)*(n+2)))
     s+=a
     if x<=1:
-        print ('a'+str(n)+'='+str(a))
+        print ('{}{}={}'.format('a', n, a))
     n+=1
 else:
     while abs(a)>=0.0001:
         a=float(x/(math.sqrt(n)*(n+2)))
         s+=a
         if x<=1:
-            print ('a'+str(n)+'='+str(a))
+            print ('{}{}={}'.format('a', n, a))
+            
         n+=1
-print ('Sum='+str(s))
+print ('{} {} {}={}'.format('Sum of first', n-1, 'a', s))
+print ('Sum of first '+str(n-1)+' a='+str(s))
