81f25da6a1c85058ba73f2e765390490973b55af
Egor <egor.vas2004@gmail.com>
2021-10-10 19:15:13 +0300

    added output

--- Lab4C++/Lab4C++.cpp
+++ Lab4C++/Lab4C++.cpp
@@ -2,6 +2,7 @@
 //
 
 #include <iostream>
+#include <stdio.h>
 using namespace std;
 
 int main()
@@ -12,15 +13,14 @@ int main()
     float xn;
     int n;
     cout << "Enter n" << endl; cin >> n;
+    cout << "x1=" << x1 << endl;
+    cout << "x2=" << x2 << endl;
     for (int i = 3; i <= n; i++)
     {
         xn = (i + 1) * tmp;
         tmp = x2;
         x2 = xn;
-        cout << "x=" << xn << endl;
-        
-        
- 
+        cout << "x" << i << "=" << xn << endl;
     }
     
 
